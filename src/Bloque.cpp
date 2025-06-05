3e0cf78884ed353108c34f065db9b70fb4047c12
Jesus-Quinteros <jquinteros@unsa.edu.pe>
2025-06-05 09:12:26 -0500

    Update Bloque.cpp

--- src/Bloque.cpp
+++ src/Bloque.cpp
@@ -1,1 +1,12 @@
+#include "Bloque.hpp"
+
+Bloque::Bloque(int id, const std::vector<Sector>& sectores) : id(id), sectores(sectores) {}
+
+int Bloque::getId() const {
+  return id;
+}
+
+const std::vector<Sector>& Bloque::getSectores() const {
+  return sectores;
+}
 
